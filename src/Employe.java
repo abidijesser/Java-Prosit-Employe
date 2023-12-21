615837b0a48c42b111192842a2f9be06d73b84ff
jasser <jasserabidi00@gmail.com>
2023-12-21 14:38:36 +0100

    1

---
+++ src/Employe.java
@@ -0,0 +1,80 @@
+public class Employe implements Comparable<Employe> {
+
+    protected int id;
+    protected String nom;
+    protected String prenom;
+    protected String nomDepartement;
+    protected int grade;
+
+    public Employe(){
+
+    }
+
+    public Employe(int id, String nom, String prenom, String nomDepartement, int grade ){
+        this.id= id;
+        this.nom=nom;
+        this.prenom=prenom;
+        this.nomDepartement=nomDepartement;
+        this.grade=grade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNomDepartement() {
+        return nomDepartement;
+    }
+    public void setNomDepartement(String nomDepartement) {
+        this.nomDepartement = nomDepartement;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+
+        if (obj == null) return false ;
+        if (this == obj) return true ;
+        if ( obj instanceof Employe employe){
+            return getId()==employe.getId() && getNom().equals(employe.getNom()) ;
+        }
+        return false ;
+    }
+
+    @Override
+    public String toString(){
+        return " Employe {id: "+id+" ,Nom: "+nom+ " ,Prenom: "+prenom+ " ,Nom de departement: "+nomDepartement+" ,grade :"+grade+ "}" ;
+    }
+
+    @Override
+    public int compareTo( Employe employe){
+        return this.id - employe.getId();
+    }
+}
---
+++ src/SocieteArrayList.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SocieteArrayList implements IGestion<Employe>{
+
+    List<Employe> employes;
+
+    public SocieteArrayList(){
+        employes= new ArrayList<>();
+    }
+
+    @Override
+    public void ajouterEmploye(Employe e){
+        employes.add(e);
+    }
+
+    @Override
+    public boolean rechercherEmploye(String nom){
+        for ( Employe e: employes){
+            if(e.getNom().equals(nom)) return true ;
+        }
+        return false ;
+    }
+
+    @Override
+    public boolean rechercherEmploye(Employe e){
+        return employes.contains(e);
+    }
+
+    @Override
+    public void supprimerEmploye(Employe e){
+        employes.remove(e);
+    }
+
+    @Override
+    public void displayEmploye(){
+        for (Employe e :employes){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void trierEmployeParId(){
+        Collections.sort(employes);
+    }
+
+    @Override
+    public void trierEmployeParNomDépartementEtGrade(){
+        Comparator<Employe> departementComparator = new Comparator<Employe>() {
+            @Override
+            public int compare(Employe o1, Employe o2) {
+                return o1.getNomDepartement().compareTo(o2.getNomDepartement());
+            }
+        };
+        Comparator<Employe> gradeComparator = new Comparator<Employe>() {
+            @Override
+            public int compare(Employe o1, Employe o2) {
+                return o1.getGrade() - o2.getGrade();
+            }
+        };
+        Collections.sort(employes, departementComparator.thenComparing(gradeComparator));
+    }
+}
